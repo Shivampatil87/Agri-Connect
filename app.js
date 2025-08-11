ea759db2fcdce627582f46e885df0edc0c27374f
Shivam Mahalle <Shivammahalle41@gmail.com>
2025-08-11 22:11:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const expressSession = require('express-session');
-
 const passport = require('passport');
 
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,9 +23,10 @@ app.use(expressSession({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Use serialize and deserialize methods from your user model
 passport.serializeUser(usersRouter.serializeUser());
 passport.deserializeUser(usersRouter.deserializeUser());
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -41,23 +38,22 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log("🚀 Server is running on http://localhost:3000");
+// Use environment port for deployment or default to 3000 locally
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
